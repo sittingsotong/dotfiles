0e4e846884830335e775ddbe234bba97ca7c2391
sittingsotong <zhaositing1026@gmail.com>
2020-09-25 15:15:21 +0100

    Added i3 scripts for brightness and lock

---
+++ .config/i3/bin/lock/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ICON=$HOME/.xlock/icon.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -i $TMPBG
+rm /tmp/screen.png
